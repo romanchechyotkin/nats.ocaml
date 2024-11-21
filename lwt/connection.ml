ec9c3f8b0a22ca3413512ba23f16e4e111691f0e
dx3mod <dx3mod@bk.ru>
2024-11-21 12:27:50 +0300

    Refactor connection creating by Lwt_io abstraction

--- lwt/connection.ml
+++ lwt/connection.ml
@@ -5,11 +5,7 @@
 open Unix
 open Nats_client
 
-type t = {
-  ic : Lwt_io.input_channel;
-  oc : Lwt_io.output_channel;
-  socket : Lwt_unix.file_descr;
-}
+type t = { ic : Lwt_io.input_channel; oc : Lwt_io.output_channel }
 (** NATS client connection. *)
 
 exception Connection_refused
@@ -23,29 +19,20 @@ exception Err_response of string
     @raises Connection_refused *)
 let create ?switch ~host ~port () =
   try%lwt
-    (* Create a TCP socket *)
-    let socket_fd = Lwt_unix.socket PF_INET SOCK_STREAM 0 in
+    let%lwt ic, oc =
+      Lwt_io.open_connection @@ ADDR_INET (inet_addr_of_string host, port)
+    in
 
-    (* Server details *)
-    let server_address = inet_addr_of_string host in
-    let server_port = port in
-    let server_socket_address = ADDR_INET (server_address, server_port) in
+    (* Automatic close socket on switch destroy. *)
+    Lwt_switch.add_hook switch (fun () -> Lwt_io.close ic);
 
-    Lwt_unix.connect socket_fd server_socket_address;%lwt
-    Lwt_switch.add_hook switch (fun () -> Lwt_unix.close socket_fd);
-
-    (* Wrap raw file descriptors into channel abstractions. *)
-    let ic = Lwt_io.of_fd ~mode:Lwt_io.Input socket_fd in
-    let oc = Lwt_io.of_fd ~mode:Lwt_io.Output socket_fd in
-
-    (* The socket_fd capture is needed to close it later. *)
-    Lwt.return { ic; oc; socket = socket_fd }
+    Lwt.return { ic; oc }
   with Unix.Unix_error (Unix.ECONNREFUSED, "connect", "") ->
     raise Connection_refused
 
-let close conn =
+let close { oc; _ } =
   (* is it work? *)
-  Lwt_unix.close conn.socket
+  Lwt_io.close oc
 
 exception Invalid_response of string
 
